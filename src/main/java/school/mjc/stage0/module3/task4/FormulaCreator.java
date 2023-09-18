70b3566ac23c87b0c737ad8f45165d8f0b592414
zahrablend <zahra@LAPTOP-010BK67M>
2023-09-18 10:05:22 +0300

    Formula task updated

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        char a = 1;
-        char b = 2;
+        char a = 0;
+        char b = 0;
 
         double result = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((double) (a + b - 4 * a * b) / 2);
         System.out.println(result);
