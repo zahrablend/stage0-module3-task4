10bdbd1018b91bb3375a89a5bde53927e1e6794a
zahrablend <zahra@LAPTOP-010BK67M>
2023-09-18 09:54:52 +0300

    Tasks completed

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        char a = 1;
+        char b = 2;
+
+        double result = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((double) (a + b - 4 * a * b) / 2);
+        System.out.println(result);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 3;
+        char h = 8;
+        char a = 1;
+        char r = 18;
+
+        System.out.println(c + "\n" + h + "\n" + a + "\n" + r);
     }
 }
